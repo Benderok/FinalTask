ffa3042f5a444bc1b80afcb6c1cc1edcc43e8f0a
Valentin Panferin <valentinpanferin@MacBook-Pro-Valentin.local>
2022-12-28 17:42:25 +0700

    correction applied

--- Task_1/1_0/Program.cs
+++ Task_1/1_0/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует
+// массив из строк, длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+void Words(string[] args)
+    {
+      string[] massive;
+      int count;
+      string s;
+      string[] massive_2;
+      string[] massive_3;
+
+      Console.WriteLine("Enter any words or symbols: ");
+
+      count = 0;
+      massive = new string[count];
+        //ввод массива с консоли
+      do
+      {
+        s = Console.ReadLine();
+        if (s!="")
+        {
+          count++;
+          massive_2 = new string[count];
+          for (int i = 0; i < massive_2.Length - 1; i++)
+            massive_2[i] = massive[i];
+          massive_2[count - 1] = s;
+          massive = massive_2;
+        }
+        }
+      while (s != "");
+    //печать массива
+    int size = massive.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{massive[i]}, ");
+    }
+    Console.WriteLine();
+    // перебор массива и создание нового массива_3
+    for (int i = 0; i < massive.Length; i++)
+        {
+        count = 0;
+        if (massive[i].Length <= 3)
+        count++;
+        massive_3 = new string[count];
+        for (int j = 0; j < massive_3.Length; j++)
+        massive_3[j] = massive[i];
+        //вывод нового массива_3
+        int size_2 = massive_3.Length;
+        for (int j = 0; j < size_2; j++)
+        Console.Write($"{massive_3[j]}, ");
+        }
+        Console.WriteLine();
+    }
+
+Words(args);
